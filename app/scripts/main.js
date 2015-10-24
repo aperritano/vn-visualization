db51635df484c882571454fffd32a4a9d5528043
Anthony Perritano <aperritano@gmail.com>
2015-10-24 15:19:59 -0500

    DRAWS CENTERS ON THE MAP!!!!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,3 @@
-
 /*global L, Firebase, d3, moment, topojson*/
 
 //var tooltip;
@@ -22,11 +21,9 @@ var wHeight = 'innerHeight' in window ? window.innerHeight : document.documentEl
 //var mainOffset = 15;
 
 var margin = {top: 100, right: 10, bottom: 150, left: 10};
-//var mainTimelineWidth = window.innerWidth - margin.left - margin.right;
 var mainTimelineInnerWidth = window.innerWidth - margin.left - margin.right - 10;
 var mainTimelineHeight = 45;
 var mainTimelineInnerHeight = mainTimelineHeight - 15;
-//var mainTimelinePostionY = window.innerHeight - (mainTimelineHeight + mainOffset);
 
 
 var mapSVG;
@@ -37,8 +34,7 @@ var map;
 var ref = new Firebase('https://gpsdatababoons.firebaseio.com/timestamps');
 
 
-
-ref.orderByKey().startAt('0').endAt('100').on('value', function (snapshot) {
+ref.orderByKey().startAt('0').endAt('10000').on('value', function (snapshot) {
     var v = snapshot.val();
     updateDataFromDB(v);
 }, function (errorObject) {
@@ -55,6 +51,105 @@ initMapLeaflet();
 
 /*** functions ******/
 
+/**
+ * updates when firebase returns
+ *
+ * @param items
+ */
+function updateDataFromDB(items) {
+
+    //there is no timeline available create one
+    if (gpsDataset === undefined || gpsDataset[0] === undefined) {
+
+        gpsDataset = items;
+
+        //create map
+        initLeafletOverlays();
+        //create timeline
+        createTimeLine();
+    } else {
+        //update timeline
+        //update map
+    }
+
+}
+
+/**
+ * Creates the Leaflet with the first datapoint
+ *
+ * @param dataPoint
+ */
+function initLeafletOverlays() {
+    //addCountryOverlay();
+    initDataPointOverlay();
+}
+
+function initDataPointOverlay() {
+
+    var firstPoint = gpsDataset[0];
+
+
+    if( firstPoint.items !== undefined ) {
+        drawDataPointOverlay(firstPoint);
+    } else {
+        //this point doesn't have subjects
+    }
+}
+
+function drawDataPointOverlay(dataPoint) {
+    var subjects = dataPoint.items;
+
+
+
+    var i = 0;
+    subjects.forEach(function (d) {
+
+        d.LatLng = new L.LatLng(d.lat, d.lon);
+
+        if( i === 0) {
+            map.panTo(d.LatLng, { animate: true });
+            map.setZoom( 16, { animate: true });
+        }
+
+        i++;
+
+    });
+
+    var circles = mapContainer.selectAll('circle')
+        .data(subjects)
+        .enter()
+        .append('circle')
+        .attr('class', 'node')
+        .attr('id', function (d) {
+            return d.id;
+        })
+        .attr('r', 10)
+        .style('fill', function (d) {
+            return nodeColorMap(d.id);
+        })
+        .style('fill-opacity', 0.7)
+        .on('click', function (d) {
+            window.alert(d);
+        });
+
+    map.on('viewreset', update);
+
+    update();
+
+    function update() {
+        circles.attr('transform',
+            function (d) {
+                return 'translate(' +
+                    map.latLngToLayerPoint(d.LatLng).x + ',' +
+                    map.latLngToLayerPoint(d.LatLng).y + ')';
+            }
+        );
+    }
+}
+
+/**
+ * Init the leaflet portion
+ */
 function initMapLeaflet() {
 
     var m = document.getElementById('map');
@@ -65,21 +160,36 @@ function initMapLeaflet() {
     m.style.width = mapLeafletWidth + 'px';
     m.style.height = mapLeafletHeight + 'px';
 
-    map = L.map('map', {zoomControl: true, attributionControl: false}).setView([0.3509073, 36.9229031], 5);
+    map = L.map('map', {zoomControl: true, attributionControl: false}).setView([0.3509073, 36.9229031], 10);
     map.touchZoom.disable();
     map.doubleClickZoom.disable();
-    map.scrollWheelZoom.disable();
+    //map.scrollWheelZoom.disable();
     map.boxZoom.disable();
     map.keyboard.disable();
     map.dragging.disable();
 
+    L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+//    //var Esri_WorldGrayCanvas =
+//    L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+////            attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+//        maxZoom: 16
+//    }).addTo(map);
+
+    // Initialize the SVG layer
+    map._initPathRoot();
+
     //create control shutoff vectors
-    // create the control
+
+
     var countryOverlayControl = L.control({position: 'topleft'});
 
     countryOverlayControl.onAdd = function (map) {
-        if( debug ) {
-            console.log('countery overlay', map);
+        if (debug) {
+            console.log('country overlay', map);
         }
         var div = L.DomUtil.create('div', 'countryOverlayControl');
 
@@ -89,48 +199,34 @@ function initMapLeaflet() {
 
     countryOverlayControl.addTo(map);
 
-
+    // create the control
     document.getElementById('countryOverlayControl').addEventListener('click', handleCountryOverlayControl, false);
 
-    //var Esri_WorldGrayCanvas =
-    L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-//            attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-        maxZoom: 16
-    }).addTo(map);
 
-    // Initialize the SVG layer
-    map._initPathRoot();
-
-
-        // Add an SVG element to Leaflet’s overlay pane
+    // Add an SVG element to Leaflet’s overlay pane
 //    mapSVG = d3.select('#map').select('svg');
 
     //mapSVG = d3.select(map.getPanes().overlayPane)
     mapSVG = d3.select('#map').select('svg');
-        //.attr('width', mapLeafletWidth)
-        //.attr('height', mapLeafletHeight)
-        //.style('background-color', 'red');
+    //.attr('width', mapLeafletWidth)
+    //.attr('height', mapLeafletHeight)
+    //.style('background-color', 'red');
 
     mapContainer = mapSVG.append('g');
 
 }
 
-function handleCountryOverlayControl() {
 
-    if (this.checked) {
-        addCountryOverlay();
-    } else {
-        mapSVG.selectAll('path').remove();
-    }
-}
-
+/**
+ * Uses d3 to add counties to the overlay
+ */
 function addCountryOverlay() {
 
 
     d3.json('KEN.topojson', function (error, collection) {
 
         //unknown error, check the console
-        if(error){
+        if (error) {
             return console.log(error);
         }
 
@@ -176,42 +272,208 @@ function addCountryOverlay() {
     });
 }
 
-function addDataPoints(dataPoint) {
-    var subjects = dataPoint.items;
 
-    subjects.forEach(function(d) {
-        d.LatLng = new L.LatLng(d.lat, d.lon);
+/**
+ * Creates Overlay for turning the main layover on-off
+ */
+function handleCountryOverlayControl() {
+
+    if (this.checked) {
+        //turn it on
+        addCountryOverlay();
+    } else {
+        //unchecked turn it off
+        mapSVG.selectAll('path').remove();
+    }
+}
+
+
+/**
+ * Create the main time line
+ *
+ * @param items - that complete dataset
+ */
+function createTimeLine() {
+    var timeDomain = d3.extent(gpsDataset, function (d) {
+        return d.timestamp;
     });
 
-    var circles = mapContainer.selectAll('circle')
-        .data(subjects)
-        .enter().append('circle')
-        .style('fill', 'orange')
-        .style('fill-opacity',0.7)
-        .attr('r', 10)
-        .on('click', function(d){
-            window.alert(d);
+
+    console.log('timedomain', JSON.stringify(timeDomain));
+
+    var tStart = moment(timeDomain[0]).toDate();
+    var tEnd = moment(timeDomain[1]).toDate();
+    var t15 = moment(timeDomain[0]).add(15, 'm').toDate();
+
+    var x = d3.time.scale()
+        .domain([tStart, tEnd])
+        .range([0, mainTimelineInnerWidth]);
+
+
+
+    currentTimeRange = findDatesInRange(tStart, t15);
+
+    currentTimeRange.push(tStart, t15);
+    brush = d3.svg.brush()
+        .x(x)
+        .extent([tStart, t15])
+        .on('brushend', brushended);
+
+
+    timelineSVG = d3.select('#timeline').append('svg')
+        .attr('fill', 'blue')
+        .attr('width', mainTimelineInnerWidth + margin.left)
+        .attr('height', mainTimelineHeight)
+        .attr('transform', 'translate(' + 0 + ',' + 0 + ')');
+
+
+    //80% of the total svg height
+    mainTimelineInnerHeight = 0.7 * mainTimelineHeight;
+    var brushAreaHeight = mainTimelineInnerHeight;
+
+    //create the mainTimeLine area and position it on screen
+    var mainTimelineContainer = timelineSVG
+        .append('g')
+        .attr('transform', 'translate(' + 0 + ',' + 0 + ')');
+
+    //draw its overall rect
+    mainTimelineRect = mainTimelineContainer.append('rect');
+    mainTimelineRect
+        .attr('fill', 'none')
+        .attr('width', '100%')
+        .attr('height', mainTimelineHeight);
+
+
+    //draw the grid background
+    mainTimelineContainer
+        .append('g')
+        .append('rect')
+        .attr('class', 'grid-background')
+        .attr('width', '100%')
+        .attr('height', brushAreaHeight)
+        .attr('transform', 'translate(' + 0 + ',' + 0 + ')');
+
+    var ga = d3.svg.axis()
+        .scale(x)
+        .orient('bottom')
+        .ticks(d3.time.minutes.utc, 15)
+        .tickSize(mainTimelineInnerHeight)
+        .tickFormat('');
+
+    //create grid axis
+    mainTimelineContainer
+        .append('g')
+        .attr('class', 'x grid')
+        .attr('height', 5)
+        .attr('transform', 'translate(15,' + 0 + ')')
+        .call(ga)
+        .selectAll('.tick')
+        .classed('minor', function (d) {
+            return d.getUTCMinutes();
         });
 
-    map.on('viewreset', update);
+    var xa = d3.svg.axis()
+        .scale(x)
+        .orient('bottom')
+        .ticks(d3.time.minutes.utc, 15)
+        .tickPadding(0);
+//
+    mainTimelineContainer.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(15,' + brushAreaHeight + ')')
+        .call(xa)
+        .selectAll('text')
+        .attr('x', -12)
+        .style('text-anchor', null);
 
-    update();
 
-    function update(){
-        circles.attr('transform',
-            function(d) {
-                return 'translate('+
-                    map.latLngToLayerPoint(d.LatLng).x +','+
-                    map.latLngToLayerPoint(d.LatLng).y +')';
-            }
-        );
+    var gBrush = mainTimelineContainer.append('g')
+        .attr('class', 'brush')
+        .call(brush)
+        .call(brush.event);
+//
+    gBrush.selectAll('rect')
+        .attr('height', mainTimelineInnerHeight);
+
+
+}
+
+
+function brushended() {
+    // only transition after input
+    if (!d3.event.sourceEvent) {
+        return;
+    }
+    var extent0 = brush.extent();
+    var extent1 = extent0.map(d3.time.minute.utc.round);
+
+    // if empty when rounded, use floor & ceil instead
+    if (extent1[0] >= extent1[1]) {
+        extent1[0] = d3.time.minute.utc.floor(extent0[0]);
+        extent1[1] = d3.time.minute.utc.ceil(extent0[1]);
+    }
+
+    d3.select(this).transition()
+        .call(brush.extent(extent1))
+        .call(brush.event);
+    console.log('brush dates', JSON.stringify(extent1), moment(extent1[0]).toDate(), moment(extent1[1]).toDate());
+
+    if (extent1[0] !== undefined && extent1[1] !== undefined) {
+        var tStart = moment(extent1[0]);
+        var tEnd = moment(extent1[1]);
+        zoomToDateRange(tStart, tEnd);
     }
 
 }
 
-function createLeafletMapOverlay(dataPoint) {
-    //addCountryOverlay();
-    addDataPoints(dataPoint);
+function zoomToDateRange(tStart, tEnd) {
+    var foundDates = findDatesInRange(tStart, tEnd);
+
+    //grab the first one and zoom to that
+    if( foundDates !== undefined || foundDates[0] !== undefined ) {
+        var firstDataPoint = foundDates[0];
+        drawDataPointOverlay(firstDataPoint);
+        //map.panTo(new L.LatLng(40.737, -73.923));
+    //
+    }
+
+}
+
+function findDatesInRange(tStart, tEnd) {
+
+    var foundDates = [];
+
+    for (var i = 0; i < gpsDataset.length; i++) {
+        var dataItem = gpsDataset[i];
+        //var t1 = tStart.valueOf();
+        //var t2 = tEnd.valueOf();
+        //var tFind = moment(dataItem.timestamp).toDate().valuOf();
+        if (moment(dataItem.timestamp).isBetween(tStart, tEnd, 'minute')) {
+            foundDates.push(dataItem);
+        }
+    }
+
+    if (foundDates !== undefined && foundDates[0] !== undefined) {
+        for (var k = 0; k < foundDates.length; k++) {
+            console.log('found date: ', JSON.stringify(foundDates[k]) +'\n');
+        }
+
+        updateDataFromDB(foundDates[0]);
+    }
+
+    return foundDates;
+}
+
+/**
+ * lookup for node colors
+ */
+function nodeColorMap(index) {
+    var colors = ['Red', 'Purple', 'Deep Purple','Ingio','Light Blue', 'Cyan', 'Green', 'Teal', 'Lime', 'Yellow', 'Orange', 'Deep Orange', 'Brown', 'Grey','Blue Grey'  ];
+
+    var c = colors[index];
+
+    return palette.get(c, '400');
+
 }
 
 
@@ -343,180 +605,6 @@ function createLeafletMapOverlay(dataPoint) {
 //
 //}
 
-function updateDataFromDB(items) {
-
-    //there is no timeline available create one
-    if (gpsDataset === undefined || gpsDataset[0] === undefined) {
-
-        gpsDataset = items;
-
-        var first = gpsDataset[0];
-        //create map
-        createLeafletMapOverlay(first);
-        //create timeline
-        createTimeLine();
-    } else {
-        //update timeline
-        //update map
-    }
-
-
-}
-
-/**
- * Create the main time line
- *
- * @param items - that complete dataset
- */
-function createTimeLine() {
-    var timeDomain = d3.extent(gpsDataset, function (d) {
-        return d.timestamp;
-    });
-
-
-    console.log('timedomain', JSON.stringify(timeDomain));
-
-    var x = d3.time.scale()
-        .domain([moment(timeDomain[0]).toDate(), moment(timeDomain[1]).toDate()])
-        .range([0, mainTimelineInnerWidth]);
-
-    var t1 = moment(timeDomain[0]).toDate();
-    var t2 = moment(timeDomain[0]).add(15, 'm').toDate();
-
-    currentTimeRange = findDatesInRange(t1, t2);
-
-    currentTimeRange.push(t1, t2);
-    brush = d3.svg.brush()
-        .x(x)
-        .extent([t1, t2])
-        .on('brushend', brushended);
-
-
-    timelineSVG = d3.select('#timeline').append('svg')
-        .attr('fill', 'blue')
-        .attr('width', mainTimelineInnerWidth + margin.left)
-        .attr('height', mainTimelineHeight)
-        .attr('transform', 'translate(' + 0 + ',' + 0 + ')');
-
-
-    //80% of the total svg height
-    mainTimelineInnerHeight = 0.7 * mainTimelineHeight;
-    var brushAreaHeight = mainTimelineInnerHeight;
-
-    //create the mainTimeLine area and position it on screen
-    var mainTimelineContainer = timelineSVG
-        .append('g')
-        .attr('transform', 'translate(' + 0 + ',' + 0 + ')');
-
-    //draw its overall rect
-    mainTimelineRect = mainTimelineContainer.append('rect');
-    mainTimelineRect
-        .attr('fill', 'none')
-        .attr('width', '100%')
-        .attr('height', mainTimelineHeight);
-
-
-    //draw the grid background
-    mainTimelineContainer
-        .append('g')
-        .append('rect')
-        .attr('class', 'grid-background')
-        .attr('width', '100%')
-        .attr('height', brushAreaHeight)
-        .attr('transform', 'translate(' + 0 + ',' + 0 + ')');
-
-    var ga = d3.svg.axis()
-        .scale(x)
-        .orient('bottom')
-        .ticks(d3.time.minutes.utc, 15)
-        .tickSize(mainTimelineInnerHeight)
-        .tickFormat('');
-
-//        //create grid axis
-    mainTimelineContainer
-        .append('g')
-        .attr('class', 'x grid')
-        .attr('height', 5)
-        .attr('transform', 'translate(15,' + 0 + ')')
-        .call(ga)
-        .selectAll('.tick')
-        .classed('minor', function (d) {
-            return d.getUTCMinutes();
-        });
-
-    var xa = d3.svg.axis()
-        .scale(x)
-        .orient('bottom')
-        .ticks(d3.time.minutes.utc, 15)
-        .tickPadding(0);
-//
-    mainTimelineContainer.append('g')
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(15,' + brushAreaHeight + ')')
-        .call(xa)
-        .selectAll('text')
-        .attr('x', -12)
-        .style('text-anchor', null);
-
-
-    var gBrush = mainTimelineContainer.append('g')
-        .attr('class', 'brush')
-        .call(brush)
-        .call(brush.event);
-//
-    gBrush.selectAll('rect')
-        .attr('height', mainTimelineInnerHeight);
-
-
-}
-
-
-function brushended() {
-    // only transition after input
-    if(!d3.event.sourceEvent) {
-        return;
-    }
-    var extent0 = brush.extent();
-    var extent1 = extent0.map(d3.time.minute.utc.round);
-
-    // if empty when rounded, use floor & ceil instead
-    if (extent1[0] >= extent1[1]) {
-        extent1[0] = d3.time.minute.utc.floor(extent0[0]);
-        extent1[1] = d3.time.minute.utc.ceil(extent0[1]);
-    }
-
-    d3.select(this).transition()
-        .call(brush.extent(extent1))
-        .call(brush.event);
-    console.log('brush dates', JSON.stringify(extent1), moment(extent1[0]).toDate(), moment(extent1[1]).toDate());
-
-    if (extent1[0] !== undefined && extent1[1] !== undefined) {
-        findDatesInRange(moment(extent1[0]), moment(extent1[1]));
-    }
-
-}
-
-function findDatesInRange(date1, date2) {
-
-    var foundDates = [];
-
-    for (var i = 0; i < gpsDataset.length; i++) {
-        var dataItem = gpsDataset[i];
-        if (moment(dataItem.timestamp).isBetween(date1, date2, 'minute')) {
-            foundDates.push(dataItem);
-        }
-    }
-
-    if (foundDates !== undefined && foundDates[0] !== undefined) {
-        for (var k = 0; k < foundDates.length; k++) {
-            console.log('found date: ', JSON.stringify(foundDates[k]));
-        }
-
-        updateDataFromDB(foundDates[0]);
-    }
-
-    return foundDates;
-}
 
 //function playPointRange() {
 //    console.log('Play Point Range');
